5c24731e2137d318691ff05671cf16c4df22e539
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-08 23:13:12 +0100

    changed variable names

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import './App.scss';
 
 function App() {
+	const [message, setMessage] = useState('');
+
 	return (
 		<div className="App">
 			<h1>Info Site</h1>
 			<p>Welcome to this site.</p>
+
+			<input
+				data-cy="input1"
+				value={message}
+				onChange={(e) => setMessage(e.target.value)}
+			/>
+			<div className="message">
+				You have <span data-cy="chars1">{message.length}</span>{' '}
+				characters.
+			</div>
 		</div>
 	);
 }
